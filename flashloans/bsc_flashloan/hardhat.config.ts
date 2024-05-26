ca1bf80314eedfc0b7227b38a6630437b9504d07
nikola <nikolatrajkovic.contact@gmail.com>
2024-05-26 15:26:13 +0200

    chore: setup hardhat networks configuration

--- flashloans/bsc_flashloan/hardhat.config.ts
+++ flashloans/bsc_flashloan/hardhat.config.ts
@@ -3,9 +3,12 @@ import "@nomiclabs/hardhat-ethers";
 import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
+import dotenv from "dotenv";
+dotenv.config();
 
-const FORKING_URL = "https://rpc.ankr.com/bsc";
-const BLOCK_NUMBER = 15216420;
+const mainnet_provider_url = process.env.MAINNET_PROVIDER_URL || "";
+const testnet_provider_url = process.env.TESTNET_PROVIDER_URL || "";
+const private_key = process.env.ACCOUNT_PRIVATE_KEY || "";
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -13,10 +16,20 @@ const config: HardhatUserConfig = {
   },
   networks: {
     hardhat: {
+      // hardhat means local network
       forking: {
-        url: FORKING_URL,
-        blockNumber: BLOCK_NUMBER,
+        url: mainnet_provider_url,
       },
+    },
+    testnet: {
+      url: testnet_provider_url,
+      chainId: 97,
+      accounts: [private_key],
+    },
+    mainnet: {
+      url: mainnet_provider_url,
+      chainId: 57,
+      accounts: [private_key],
     },
   },
 };
